7e2bc1a6d3434282f81e03dd3fc8922999ee5136
junaid shaikh <junaidlatur@gmail.com>
2019-05-01 07:16:07 +0530

    cfg_to_dict.py Update

--- README.txt
+++ README.txt
@@ -2,7 +2,9 @@ Implimentation of YOLO v3 from scratch using Pytorch
 
 Directory Structure
 	- main  ---- config	(contains configuration of YOLO v3).
-		.
+			
 		---- utils	(contains utils for training of object detector from scratch).
-	
+			|
+			|----- Darknet.py (this script is used to converet all config to a neural network).
+
 	
---
+++ utils/cfg_to_dict.py
@@ -0,0 +1,26 @@
+''''
+    Author: 
+    
+    Date:
+
+    Project: Object detection using py-Torch.
+    
+    This file is used to read ./config/yolov3.cfg to a and then convert this config file to dictionary.
+
+    Dictionary should contain key as a type of fuction and value as value.
+
+    e.g.  {'layer': 'convolutional', 'batch_normalize': '1', 'filters': '32', 'size': '3', 'stride': '1', 'pad': '1', 'activation': 'leaky'}
+    
+''' 
+
+
+''''
+    This funtion should convert config file to dictionary
+
+    Input:  cfg_file (string):      Address of the config file.
+    Output: config_dict(dictonary): Dictonary containing configurations.
+''' 
+
+def cfg_to_dict(cfg_file):
+
+    pass
